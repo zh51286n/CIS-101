2f56fb2a7f3aa9b976b28508ca9e951f953b13d4
zh51286n <58083458+zh51286n@users.noreply.github.com>
2019-11-22 11:56:17 -0500

    Add files via upload

---
+++ HangZMadLibs.py
@@ -0,0 +1,8 @@
+color = input("Enter your favourite color ")
+place = input("Enter your favourite place ")
+cloth = input("Enter your favourite cloth ")
+sport = input("What's your favourite sport? ")
+season = input("What's your favourite season? ")
+print("I always " +sport + " in the " + place)
+print("I love wearing " + color + " " + cloth + " in " + season)
+print("On an " + season + " morning, I was wearing " + color + " " + cloth + " and " + sport + " in the  " + place)
